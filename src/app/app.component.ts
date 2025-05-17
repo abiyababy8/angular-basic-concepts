88c8238c5d97b22d1e38b8446040753da70d7fdd
abiyababy8 <abiyababy16@gmail.com>
2025-05-17 12:40:47 +0530

    added some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DummyComponent } from './dummy/dummy.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, DummyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
