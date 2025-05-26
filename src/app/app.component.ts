fe5c2ba44836ec3f371959b4fd25236766708acd
abiyababy8 <abiyababy16@gmail.com>
2025-05-26 11:05:44 +0530

    added some code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DummyComponent } from './dummy/dummy.component';
+//import { StudentComponent } from './student/student.component';
 
 @Component({
   selector: 'app-root',
