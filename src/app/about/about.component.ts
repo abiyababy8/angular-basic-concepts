26148b5d8daa0fd2c3dacf06188bd82d8198c900
abiyababy8 <abiyababy16@gmail.com>
2025-05-29 11:53:27 +0530

    added some code

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { UserComponent } from '../user/user.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
-  imports: [UserComponent],
+  imports: [UserComponent, FormsModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  //define property for holding value of each input box
+  userEmail: string = ""
+  firstname: string = ""
+  password: string = ""
   dataToSend = "Sean"
   getMessage(data: any) {
     alert(data)
+  }
+  register(){
+    alert(`${this.userEmail}, ${this.firstname}, ${this.password}`)
   }
 }
